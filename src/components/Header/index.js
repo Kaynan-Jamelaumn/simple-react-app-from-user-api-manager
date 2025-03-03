579a19593cb7ed58efe2ad68cdae5e59e2e3bdb1
Kaynan Felipe Rodrigues Vieira <kaynanrodrigues.nt@gmail.com>
2025-03-02 23:07:28 -0300

    Feature: login/logout button appears dynamically depending on whether the user is authenticated or not

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt, FaMoon, FaSun } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaMoon, FaSun, FaSignOutAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../../context/ThemeContext';
-import { Nav, NavLink, ThemeToggle, NavItems  } from './styled';
-import { darkTheme } from '../../config/theme'; 
+import { Nav, NavLink, ThemeToggle, NavItems } from './styled';
+import { darkTheme } from '../../config/theme';
+import { useAuth } from '../../context/AuthContext';
 
 export default function Header() {
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const isDarkTheme = theme === darkTheme; 
+  const { isAuthenticated, logout } = useAuth(); // Use useAuth to get authentication state and logout function
+  const isDarkTheme = theme === darkTheme;
 
   return (
     <Nav style={{ backgroundColor: theme.navBackGround, color: theme.iconColor }}>
@@ -15,9 +17,16 @@ export default function Header() {
         <NavLink as={Link} to="/">
           <FaHome size={24} />
         </NavLink>
-        <NavLink as={Link} to="/login">
-          <FaSignInAlt size={24} />
-        </NavLink>
+        {/* Conditionally render Login or Logout button */}
+        {!isAuthenticated ? (
+          <NavLink as={Link} to="/login">
+            <FaSignInAlt size={24} />
+          </NavLink>
+        ) : (
+          <NavLink as={Link} to="/" onClick={logout}>
+            <FaSignOutAlt size={24} />
+          </NavLink>
+        )}
         <NavLink as={Link} to="/profile">
           <FaUserAlt size={24} />
         </NavLink>
